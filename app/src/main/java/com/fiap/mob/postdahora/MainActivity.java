1ded28cba5dae0c7b6b19d82b6d1c0a62cfbd47f
doido <sjmartini@terra.com.br>
2017-01-23 22:43:45 -0200

    commit

--- app/src/main/java/com/fiap/mob/postdahora/MainActivity.java
+++ app/src/main/java/com/fiap/mob/postdahora/MainActivity.java
@@ -1,32 +1,65 @@
 package com.fiap.mob.postdahora;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.facebook.login.widget.ProfilePictureView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import static android.R.attr.data;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edtEmail;
+    private EditText edtFbName;
+    private ProfilePictureView profilePictureView;
     CallbackManager callbackManager;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //FacebookSdk.sdkInitialize(getApplicationContext());
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        edtEmail = (EditText) findViewById(R.id.edtEmail);
+        edtFbName = (EditText) findViewById(R.id.edtFbName);
+        profilePictureView = (ProfilePictureView) findViewById(R.id.profPic);
         callbackManager = CallbackManager.Factory.create();
         LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
 
 
             @Override
             public void onSuccess(LoginResult loginResult) {
+
+                GraphRequest request = GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+                                Log.v("Main", response.toString());
+                                configurarProfile(object);
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender, birthday");
+                request.setParameters(parameters);
+                request.executeAsync();
 
             }
 
@@ -42,5 +75,28 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+    }
+
+
+    private void configurarProfile(JSONObject jsonObject){
+
+        try{
+            Log.i(jsonObject.getString("name"), "NOME");
+            edtEmail.setText(jsonObject.getString("email"));
+            edtFbName.setText(jsonObject.getString("name"));
+            profilePictureView.setPresetSize(ProfilePictureView.NORMAL);
+            profilePictureView.setProfileId(jsonObject.getString("id"));
+
+
+
+        }catch (JSONException e){
+
+            e.printStackTrace();
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
     }
